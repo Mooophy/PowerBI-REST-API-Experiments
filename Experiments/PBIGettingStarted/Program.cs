ee31398d67360afc5ec7aea753e6d03f85f87cdd
Yue Wang <yue.wang@aderant.com>
2016-12-02 09:11:40 +1300

    polished

--- Experiments/PBIGettingStarted/Program.cs
+++ Experiments/PBIGettingStarted/Program.cs
@@ -5,10 +5,8 @@ using System.IO;
 using System.Web.Script.Serialization;
 using System.Collections.Generic;
 
-namespace PBIGettingStarted
-{
-    public class Dataset
-    {
+namespace PBIGettingStarted {
+    public class Dataset {
         private static JavaScriptSerializer Serializer { get; } = new JavaScriptSerializer();
         public string name { get; set; }
         public List<Table> tables { get; set; }
@@ -16,20 +14,17 @@ namespace PBIGettingStarted
         public string ToJson() => Serializer.Serialize(this);
     }
 
-    public class Table
-    {
+    public class Table {
         public string name { get; set; }
         public List<Column> columns { get; set; }
     }
 
-    public class Column
-    {
+    public class Column {
         public string name { get; set; }
         public string dataType { get; set; }
     }
 
-    public class Relationship
-    {
+    public class Relationship {
         public string name { get; set; }
         public string crossFilteringBehavior { get; set; } = "OneDirection";
         public string fromTable { get; set; }
@@ -38,67 +33,64 @@ namespace PBIGettingStarted
         public string toColumn { get; set; }
     }
 
-    class Program
-    {
+    class Program {
         private static readonly string ClientId = Properties.Resources.ClientId;
         //RedirectUri you used when you registered your app.
         //For a client app, a redirect uri gives AAD more details on the specific application that it will authenticate.
-        private static string redirectUri = "https://login.live.com/oauth20_desktop.srf";
+        private const string RedirectUri = "https://login.live.com/oauth20_desktop.srf";
         //Resource Uri for Power BI API
-        private static string resourceUri = "https://analysis.windows.net/powerbi/api";
+        private const string ResourceUri = "https://analysis.windows.net/powerbi/api";
         //OAuth2 authority Uri
-        private static string authority = "https://login.windows.net/common/oauth2/authorize";
+        private const string Authority = "https://login.windows.net/common/oauth2/authorize";
         private static AuthenticationContext authContext;
         private static string token = string.Empty;
 
         //Uri for Power BI datasets
-        private static string datasetsUri = "https://api.powerbi.com/v1.0/myorg";
+        private const string DatasetsUri = "https://api.powerbi.com/v1.0/myorg";
 
         //Example dataset name and group name
-        private static string datasetName = "Data Set For Experiment Created at " + DateTime.Now.ToUniversalTime();
+        private static readonly string DatasetName = "Data Set For Experiment Created at " + DateTime.Now.ToUniversalTime();
 
-        static void Main(string[] args)
-        {
-            var ds = new Dataset
-            {
-                name = datasetName,
+        private static void Main() {
+            var ds = new Dataset {
+                name = DatasetName,
                 tables = new List<Table>
+                {
+                    new Table
                     {
-                        new Table
+                        name = "Ages",
+                        columns = new List<Column>
                         {
-                            name = "Ages",
-                            columns = new List<Column>
+                            new Column
                             {
-                                new Column
-                                {
-                                    name = "Id",
-                                    dataType = "Int64"
-                                },
-                                new Column
-                                {
-                                    name = "Age",
-                                    dataType = "Int64"
-                                }
-                            }
-                        },
-                        new Table
-                        {
-                            name = "Names",
-                            columns = new List<Column>
+                                name = "Id",
+                                dataType = "Int64"
+                            },
+                            new Column
                             {
-                                new Column
-                                {
-                                    name = "Id",
-                                    dataType = "Int64"
-                                },
-                                new Column
-                                {
-                                    name = "Name",
-                                    dataType = "string"
-                                }
+                                name = "Age",
+                                dataType = "Int64"
                             }
-                        },
+                        }
                     },
+                    new Table
+                    {
+                        name = "Names",
+                        columns = new List<Column>
+                        {
+                            new Column
+                            {
+                                name = "Id",
+                                dataType = "Int64"
+                            },
+                            new Column
+                            {
+                                name = "Name",
+                                dataType = "string"
+                            }
+                        }
+                    },
+                },
                 relationships = new List<Relationship>
                 {
                     new Relationship
@@ -113,13 +105,10 @@ namespace PBIGettingStarted
                 }
             };
 
-            try
-            {
-                HttpWebRequest request = CreateRequestForDataset($"{datasetsUri}/datasets", "POST", AccessToken());
+            try {
+                var request = CreateRequestForDataset($"{DatasetsUri}/datasets", "POST", AccessToken());
                 Console.WriteLine(PostRequest(request, ds.ToJson()));
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 Console.WriteLine(ex.Message);
             }
             Console.ReadKey();
@@ -129,49 +118,40 @@ namespace PBIGettingStarted
         /// Use AuthenticationContext to get an access token
         /// </summary>
         /// <returns></returns>
-        static string AccessToken()
-        {
-            if (token == String.Empty)
-            {
+        private static string AccessToken() {
+            if (token == string.Empty) {
                 //Get Azure access token
                 // Create an instance of TokenCache to cache the access token
-                TokenCache TC = new TokenCache();
+                var tc = new TokenCache();
                 // Create an instance of AuthenticationContext to acquire an Azure access token
-                authContext = new AuthenticationContext(authority, TC);
+                authContext = new AuthenticationContext(Authority, tc);
                 // Call AcquireToken to get an Azure token from Azure Active Directory token issuance endpoint
-                token = authContext.AcquireToken(resourceUri, ClientId, new Uri(redirectUri), PromptBehavior.RefreshSession).AccessToken;
-            }
-            else
-            {
+                token = authContext.AcquireToken(ResourceUri, ClientId, new Uri(RedirectUri), PromptBehavior.RefreshSession).AccessToken;
+            } else {
                 // Get the token in the cache
-                token = authContext.AcquireTokenSilent(resourceUri, ClientId).AccessToken;
+                token = authContext.AcquireTokenSilent(ResourceUri, ClientId).AccessToken;
             }
 
             return token;
         }
-        private static string PostRequest(HttpWebRequest request, string json)
-        {
+        private static string PostRequest(HttpWebRequest request, string json) {
             byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(json);
             request.ContentLength = byteArray.Length;
 
             //Write JSON byte[] into a Stream
-            using (Stream writer = request.GetRequestStream())
-            {
+            using (var writer = request.GetRequestStream()) {
                 writer.Write(byteArray, 0, byteArray.Length);
             }
 
             return GetResponse(request);
         }
 
-        private static string GetResponse(HttpWebRequest request)
-        {
-            string response = string.Empty;
+        private static string GetResponse(HttpWebRequest request) {
+            string response;
 
-            using (HttpWebResponse httpResponse = request.GetResponse() as System.Net.HttpWebResponse)
-            {
+            using (var httpResponse = request.GetResponse() as HttpWebResponse) {
                 //Get StreamReader that holds the response stream
-                using (StreamReader reader = new System.IO.StreamReader(httpResponse.GetResponseStream()))
-                {
+                using (var reader = new StreamReader(httpResponse.GetResponseStream())) {
                     response = reader.ReadToEnd();
                 }
             }
@@ -179,14 +159,13 @@ namespace PBIGettingStarted
             return response;
         }
 
-        private static HttpWebRequest CreateRequestForDataset(string datasetsUri, string method, string accessToken)
-        {
-            HttpWebRequest request = WebRequest.Create(datasetsUri) as HttpWebRequest;
+        private static HttpWebRequest CreateRequestForDataset(string datasetsUri, string method, string accessToken) {
+            var request = WebRequest.Create(datasetsUri) as HttpWebRequest;
             request.KeepAlive = true;
             request.Method = method;
             request.ContentLength = 0;
             request.ContentType = "application/json";
-            request.Headers.Add("Authorization", String.Format("Bearer {0}", accessToken));
+            request.Headers.Add("Authorization", $"Bearer {accessToken}");
             return request;
         }
     }
